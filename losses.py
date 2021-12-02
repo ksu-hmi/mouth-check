305836d715c0c06c11269e601d40e109d8952fcc
Megan Bove <mbove@students.kennesaw.edu>
2021-12-02 17:50:29 -0500

    Updated with English translation

--- losses.py
+++ losses.py
@@ -123,6 +123,9 @@ def lovasz_grad(gt_sorted):
     Computes gradient of the Lovasz extension w.r.t sorted errors
     See Alg. 1 in paper
     """
+    #Computes gradient of the Lovasz extension w.r.t sorted errors See Alg. 1 in paper
+
+
     gts = tf.reduce_sum(gt_sorted)
     intersection = gts - tf.cumsum(gt_sorted)
     union = gts + tf.cumsum(1. - gt_sorted)
@@ -139,6 +142,13 @@ def lovasz_hinge(logits, labels, per_image=True, ignore=None):
       per_image: compute the loss per image instead of per batch
       ignore: void class id
     """
+    #Binary Lovasz hinge loss
+      #logits: [B, H, W] Variable, logits at each pixel (between -infty and +infty)
+      #labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      #per_image: compute the loss per image instead of per batch
+      #ignore: void class id
+
+
     if per_image:
         def treat_image(log_lab):
             log, lab = log_lab
@@ -164,6 +174,12 @@ def lovasz_hinge_flat(logits, labels):
       labels: [P] Tensor, binary ground truth labels (0 or 1)
       ignore: label to ignore
     """
+    #   Binary Lovasz hinge loss
+      #logits: [P] Variable, logits at each prediction (between -infty and +infty)
+      #labels: [P] Tensor, binary ground truth labels (0 or 1)
+      #ignore: label to ignore
+
+
 
     def compute_loss():
         labelsf = tf.cast(labels, logits.dtype)
