68e61df837bbecaf93aafaec7161c10641e3f7f0
Megan Bove <mbove@students.kennesaw.edu>
2021-12-02 17:52:16 -0500

    Translation to English

--- train.py
+++ train.py
@@ -141,19 +141,19 @@ if __name__ == "__main__":
     launch()
 
     #alert made by Megan to alert decay to clinician
-from tkinter import messagebox, Tk
+#from tkinter import messagebox, Tk
 
 
-def alert(title, message, kind='info', hidemain=True):
-    if kind not in ('no caries detected'):
-        raise ValueError('Diagnosis: Good')
+#def alert(title, message, kind='info', hidemain=True):
+    #if kind not in ('no caries detected'):
+        #raise ValueError('Diagnosis: Good')
 
-    show_method = getattr(messagebox, 'show{}'.format(kind))
-    show_method(title, message)
+    #show_method = getattr(messagebox, 'show{}'.format(kind))
+    #show_method(title, message)
 
 
-if __name__ == '__main__':
-    Tk().withdraw()
-    alert('No caries detected')
-    alert('Caries detected', kind='warning!')
+#if __name__ == '__main__':
+    #Tk().withdraw()
+    #alert('No caries detected')
+    #alert('Caries detected', kind='warning!')
 #end of edit to code by Megan
